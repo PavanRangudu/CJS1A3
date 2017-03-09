cd8db51e2644315ea6b45ceada7c9840f746226f
PavanRangudu <pavanharishankar@gmail.com>
2017-03-09 22:55:13 +0530

    CJS1A3_solution

---
+++ src/CJS1A3_swap_without_third_var.java
@@ -0,0 +1,11 @@
+
+public class CJS1A3_swap_without_third_var {
+	public static void main (String s[]) {
+		int a = 5, b = 7;
+		// Swaping without third variable
+		a = a + b; // a = 12 , b = 7
+		b = a - b; // a = 12 , b = 5
+		a = a - b; // a = 7 , b = 5
+		System.out.println("Swapped values a = "+a+" b = "+b);
+	}
+}
