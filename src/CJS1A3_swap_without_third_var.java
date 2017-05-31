c3b506526a74a14c42fea8a0373000503e86dc65
PavanRangudu <pavanharishankar@gmail.com>
2017-05-31 22:34:04 +0530

    output

--- src/CJS1A3_swap_without_third_var.java
+++ src/CJS1A3_swap_without_third_var.java
@@ -1,7 +1,7 @@
 
 public class CJS1A3_swap_without_third_var {
 	public static void main (String s[]) {
-		int a = 5, b = 7;
+		int a = 5, b = 7; // Initial values taken (for example)
 		// Swaping without third variable
 		a = a + b; // a = 12 , b = 7
 		b = a - b; // a = 12 , b = 5
